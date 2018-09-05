faa7ae82498c466529a3a90d8e22632b40b65e7e
gonDragon <gyarott@gmail.com>
2018-09-05 19:35:03 +0900

    Merge branch 'master' of https://github.com/gonDragonJP/Shoot2
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/gondragon/shoot2/myshot/MyShot.java
+++ app/src/main/java/com/gondragon/shoot2/myshot/MyShot.java
@@ -1,59 +1,35 @@
 package com.gondragon.shoot2.myshot;
 
+import android.graphics.Point;
+
+import com.gondragon.shoot2.Global;
+import com.gondragon.shoot2.vector.Double2Vector;
+
 public class MyShot {
 
-    protected MyPlane plane;
-    private MyShotGenerator shotGenerator;
+    public int x, y;
+    public Double2Vector velocity = new Double2Vector();
+    public float angle;
 
-    public int x;
-    public int y;
     public boolean isInScreen;
     public boolean isInExplosion;
     public boolean isLaser;
+
     public int shotPower, shotRadius;
 
-    protected Double2Vector velocity = new Double2Vector();
+    MyShotDrawer drawer;
 
-    protected float angle;
+    public MyShot(){
 
-
-    public MyShot(ObjectsContainer objectsContainer){
-
-        animationManager = objectsContainer.animationManager;
-        plane = objectsContainer.plane;
-        shotGenerator = objectsContainer.shotGenerator;
+        drawer = new MyShotDrawer();
 
         initialize();
     }
 
     private void initialize(){
 
-        setScreenLimit();
-
         isInScreen = true;
         isInExplosion = false;
-    }
-
-    private void setScreenLimit(){
-
-        screenLimit.left  = (int)Global.virtualScreenLimit.left;
-        screenLimit.right = (int)Global.virtualScreenLimit.right;
-        screenLimit.top   = 0;// 画面外上部無効　(int)Global.virtualScreenLimit.top;
-        screenLimit.bottom= (int)Global.virtualScreenLimit.bottom;
-    }
-
-    public void setShape(boolean isLaser){
-
-        this.isLaser = isLaser;
-
-        if(isLaser){
-            animeSet = animationManager.getAnimationSet
-                    (AnimationManager.AnimeObject.MYLASER, 0);
-        }
-        else{
-            animeSet = animationManager.getAnimationSet
-                    (AnimationManager.AnimeObject.MYBULLET, 0);
-        }
     }
 
     public void setVectors(Point startPos, Double2Vector velocity){
@@ -69,76 +45,18 @@ public class MyShot {
     public void setExplosion(){
 
         isInExplosion = true;
-        animeKind = AnimeKind.EXPLOSION;
-        totalAnimeFrame = 0;
-    }
-
-    public void onDraw(GL10 gl){
-
-        if(!isInExplosion){
-
-            drawCenter.set(x, y);
-
-            gl.glMatrixMode(GL10.GL_MODELVIEW);
-
-            gl.glPushMatrix();
-            {
-                gl.glLoadIdentity();
-
-                gl.glTranslatef(drawCenter.x, drawCenter.y, 0);
-                gl.glRotatef(angle, 0, 0, 1);
-
-                drawCenter.set(0, 0);
-
-                animationManager.setFrame(
-                        animeSet.getData(AnimeKind.NORMAL,0),
-                        animeFrame
-                );
-                animationManager.drawFrame(drawCenter);
-            }
-            gl.glPopMatrix();
-        }
-
-        else{
-
-            drawCenter.set(x, y);
-
-            animationManager.setFrame(
-                    animeSet.getData(AnimeKind.EXPLOSION,0),
-                    animeFrame
-            );
-            animationManager.drawFrame(drawCenter);
-        }
     }
 
     public void periodicalProcess(){
 
         flyAhead();
-        checkPositionLimit();
-        animate();
+        isInScreen = drawer.checkScreenLimit(x, y);
+        isInScreen = drawer.animate();
     }
 
-    protected void flyAhead(){
+    private void flyAhead(){
 
         x += velocity.x;
         y += velocity.y;
-    }
-
-    private void checkPositionLimit(){
-
-        if(
-                x<screenLimit.left || y<screenLimit.top ||
-                        x>screenLimit.right || y>screenLimit.bottom
-                ) isInScreen = false;
-    }
-
-    protected void animate(){
-
-        int eva = animationManager.checkAnimeLimit
-                (animeSet.getData(animeKind, 0), ++totalAnimeFrame);
-
-        if (eva == -1) isInScreen = false;
-        else
-            animeFrame = eva;
     }
 }
